a3e7531bb4e26222ff12ba4fd8bed04dc4447a4f
ylmzbrscan <ylmz_brs_can@yahoo.com>
2023-10-30 11:13:08 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '86900669-bc97-4860-8bb0-1d6c2f5ec592';
+export const MODAL_CLOSE_SEC = 2.5;
